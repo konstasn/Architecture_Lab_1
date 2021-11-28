203f7b042cbac74bdc4d3e1d79d73a3de870e0fb
konstasn <95144393+konstasn@users.noreply.github.com>
2021-11-28 17:49:54 +0200

    Add files via upload

---
+++ my_test_program/fib_test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int fib(int n)
+{
+	if(n==0||n==1) return 1;
+	else return fib(n-1) + fib(n-2);
+}
+
+int main()
+{
+	int i;
+	for(i=0;i<20;i++)
+	{
+		printf("%d\n",fib(i));
+	}
+	return 0;
+}
